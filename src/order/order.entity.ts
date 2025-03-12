e97a8c4261a94d2a86f63520107f4579ea9b2651
Ilakkiya-BQTech <ilakkiya@bqtech.in>
2025-03-12 15:38:07 +0530

    removed the comment code

--- src/order/order.entity.ts
+++ src/order/order.entity.ts
@@ -1,26 +1,5 @@
-// import { Entity, PrimaryGeneratedColumn, ManyToOne, OneToMany, Column, CreateDateColumn } from 'typeorm';
-// import { User } from '../users/user.entity';
-// import { OrderItem } from './order-item.entity';
-
-// @Entity()
-// export class Order {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @ManyToOne(() => User, (user) => user.orders)
-//   user: User;
-
-//   @OneToMany(() => OrderItem, (orderItem) => orderItem.order, { cascade: true })
-//   items: OrderItem[];
-
-//   @Column({ default: 'pending' }) 
-//   status: string;
-
-//   @CreateDateColumn()
-//   createdAt: Date;
-// }
 import { Entity, PrimaryGeneratedColumn, ManyToOne, OneToMany, Column, CreateDateColumn } from 'typeorm';
-import { User } from '../users/user.entity'; // ✅ Correct path
+import { User } from '../users/user.entity'; 
 import { OrderItem } from './order-item.entity';
 
 @Entity()
